950642d86a1b3478f8a8f061325beb391e7c68c1
Anders <1086088@ucn.dk>
2022-12-11 22:36:10 +0100

    Setting up more Setters/ Getters

--- aen-countdown_v2.js
+++ aen-countdown_v2.js
@@ -13,7 +13,6 @@ customElements.define('aen-countdown', class extends HTMLElement {
     : new Date().getTime();
   }
 
-
   get stopDate() {
     let stop = Date.parse( this.getAttribute('stop') );
     return !isNaN(stop) 
@@ -21,30 +20,74 @@ customElements.define('aen-countdown', class extends HTMLElement {
     : false;
   }
 
-  get disable() {
-
+  get disabled() {
+    return this.hasAttribute('disabled');
   }
 
-  set disable(value) {
-
+  /**
+   * @params {Boolean} state - Should the current state of the Countdown be disabled?
+   */
+  set disabled(state) {
+    let disable = Boolean(state);
+    disable
+    ? this.setAttribute('disabled','')
+    : this.removeAttribute('disabled');
   }
+
+  get time() {    
+    /**
+     * Help from this article: https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
+     */
+    if (!this.stopDate) {
+      this.disabled = true;
+      return false;
+    };
+
+    var 
+    remaining = this.stopDate - new Date().getTime(),
+    formatTime = function(time) {
+        let formated = Math.floor(time);
+        return formated < 10 
+        ? '0' + formated
+        : String(formated);
+    },
+    timeObject = {
+      days: formatTime(remaining/(24*60*60*1000)),
+      hours: formatTime((remaining/(60*60*1000)) % 24),
+      minutes: formatTime((remaining/1000/60) % 60),
+      seconds: formatTime((remaining/1000) % 60 )
+    };
+    timeObject.digits = Object.values(timeObject)
+    .reduce((previousValue, currentValue) => previousValue + currentValue)
+    .split('');
+
+    return timeObject;
+  }
+
+
   
   constructor() {
     super();
 
     const shadow = this.attachShadow({ mode: 'open' });
-
-    this.status = 'active';
    
 
   }
 
-  runCountdown() {
-    if(!this.disable)
+  _core() {
+    const loop = setInterval(_coreCallback, 1000);
+
+    if(!stop) clearInterval(loop);
+  }
+
+  _coreCallback() {
+    if (this.disabled) return;
+
   }
 
   connectedCallback() {
     console.warn(this.startDate)
+    console.warn(this.time)
   }
 
   createTimer(value) {
